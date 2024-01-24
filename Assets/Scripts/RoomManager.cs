d878d17231a72637c1b2535306782fe5a60b31d2
samira-hashemi <ladysamira88@gmail.com>
2024-01-25 02:30:41 +0330

    add photon + add movement + fix camera on player

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class  RoomManager : MonoBehaviourPunCallbacks
+{
+    public GameObject player;
+
+    [Space]
+    public Transform  spawnPoint;
+ // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster() {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected To Server!");
+
+        PhotonNetwork.JoinLobby();
+
+
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null , null);
+
+        Debug.Log("We're Connected and in a room now!");
+        
+        Invoke("PhotonInit", 10);
+    }
+
+    void PhotonInit() {
+    GameObject _player = PhotonNetwork.Instantiate (player.name, spawnPoint.position,Quaternion.identity);
+
+
+    }
+}
